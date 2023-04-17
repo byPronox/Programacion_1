bbb560ec68223c1a43ef6e93888bebb442a19d50
byPronox <108915389+byPronox@users.noreply.github.com>
2023-04-17 16:26:06 -0500

    Update EJERCICIO 3.c

--- EJERCICIO 3.c
+++ EJERCICIO 3.c
@@ -1,7 +1,5 @@
 /******************************************************************************
-
 Leer un número entero y mostrar todos los divisores exactos del número comprendidos entre 1 y el número leído.
-
 *******************************************************************************/
 #include <stdio.h>
 
@@ -10,6 +8,9 @@ int main()
     int num;
     printf("Escriba un numero: ");
     scanf("%d", &num);
+/******************************************************************************
+Se le solicita al usuario que ingrese los valores a la variable num.
+*******************************************************************************/
     int i = 0;
     while(i <= num)
     {
@@ -20,6 +21,10 @@ int main()
         }  
         
     }
+/******************************************************************************
+Se ocupa una sentencia repetitiva para aumentar en 1 en 1 hasta el numero ingresado por
+el usuario donde si se cumple la condicion se le imprime el numero al ususario.
+*******************************************************************************/
 
     return 0;
 }
