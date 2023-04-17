ed1abf0e6e00ab49e79694a6b220c33c7c263f51
byPronox <108915389+byPronox@users.noreply.github.com>
2023-04-17 15:59:40 -0500

    Update EJERCICIO 3.c

--- EJERCICIO 3.c
+++ EJERCICIO 3.c
@@ -2,7 +2,19 @@
 
 int main()
 {
-    printf("Hello World");
+    int num;
+    printf("Escriba un numero: ");
+    scanf("%d", &num);
+    int i = 0;
+    while(i <= num)
+    {
+        i = i + 1;
+        if ( num % i == 0)
+        {
+          printf("\n %d", i);  
+        }  
+        
+    }
 
     return 0;
 }
